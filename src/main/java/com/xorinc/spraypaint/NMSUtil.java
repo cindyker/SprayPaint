95798c1c3cb4dfc58c6b5b6206f5795863989faa
cindyker <cindy_k@hotmail.com>
2014-07-19 22:59:15 -0400

    Updated for 1.7.10

--- src/main/java/com/xorinc/spraypaint/NMSUtil.java
+++ src/main/java/com/xorinc/spraypaint/NMSUtil.java
@@ -1,10 +1,10 @@
 package com.xorinc.spraypaint;
 
-import net.minecraft.server.v1_7_R3.Items;
-import net.minecraft.server.v1_7_R3.WorldMap;
+import net.minecraft.server.v1_7_R4.Items;
+import net.minecraft.server.v1_7_R4.WorldMap;
 
 import org.bukkit.World;
-import org.bukkit.craftbukkit.v1_7_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_7_R4.CraftWorld;
 import org.bukkit.map.MapView;
 
 public class NMSUtil {
@@ -12,7 +12,7 @@ public class NMSUtil {
 	
 	public static MapView createMap(short id, World w){
 		
-	    net.minecraft.server.v1_7_R3.ItemStack stack = new net.minecraft.server.v1_7_R3.ItemStack(Items.MAP, 1, id);
+	    net.minecraft.server.v1_7_R4.ItemStack stack = new net.minecraft.server.v1_7_R4.ItemStack(Items.MAP, 1, id);
 	    
 	    WorldMap worldmap = Items.MAP.getSavedMap(stack, ((CraftWorld)w).getHandle());
 	    
